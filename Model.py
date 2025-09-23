c42b7f5ac8ea1b921f574ef61ea294f5003608d6
Zahra <zahra.mos2003@gmail.com>
2025-09-23 14:37:11 +0200

    Updated the model

--- Model.py
+++ Model.py
@@ -13,6 +13,8 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
+import seaborn as sns
+
     
 
 rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
@@ -106,46 +108,66 @@ svr_lin.fit(X_train_prepared, y_train)
 
 
 #Svr non linear classifier
-svr_rbf = SVR(kernel='rbf', C=100, epsilon=0.5)
+svr_rbf = SVR(kernel='rbf', C=100, epsilon=0.5, gamma='scale')
 svr_rbf.fit(X_train_prepared, y_train)
 
-some_data = X_train.iloc[:5]
-some_labels = y_train.iloc[:5]
-some_data_prepared = full_pipeline.transform(some_data)
+#Svr Polynomial
+svr_poly = SVR(kernel='poly', degree=3, C=100, epsilon=0.5)
+svr_poly.fit(X_train_prepared, y_train)
 
+#Predictions for linear, rbf and poly.
 lin_predictions = svr_lin.predict(X_train_prepared)
 nonlinear_predictions = svr_rbf.predict(X_train_prepared)
+poly_predictions = svr_poly.predict(X_train_prepared)
 
+#Linear mean square error
 lin_mse = mean_squared_error(y_train, lin_predictions)
 lin_rmse = np.sqrt(lin_mse)
 
+#Rbf mean square error
 rbf_mse = mean_squared_error(y_train, nonlinear_predictions)
 rbf_rmse = np.sqrt(rbf_mse)
 
+#Polynomial mean square error
+poly_mse = mean_squared_error(y_train, poly_predictions)
+poly_rmse = np.sqrt(poly_mse)
 
+# RMSE results
 """print("Linear prediction: ",lin_rmse)
 print("Non linear prediction: ",rbf_rmse)
-"""
+print("Polynomial prediction: ",poly_rmse)"""
+
 
 # Fine Tuning with search grid
-param_grid = [
+"""param_grid = [
     {"kernel": ["linear"],
      "C": [1, 10, 100],
      "epsilon": [0.1, 0.2, 0.5]}
-]
+]"""
+"""param_grid = [
+    {"kernel": ["rbf"],
+     "C": [1, 10, 100],
+     "epsilon": [0.1, 0.2, 0.5]}
+]"""
+"""param_grid = [
+    {"kernel": ["poly"],
+     "C": [1, 10, 100],
+     "epsilon": [0.1, 0.2, 0.5]}
+]"""
 
-grid_search = GridSearchCV(SVR(), param_grid, cv=5,
+"""grid_search = GridSearchCV(SVR(), param_grid, cv=5,
                            scoring="neg_mean_squared_error",
                            verbose=2)
 grid_search.fit(X_train_prepared, y_train)
 
-#print("Best params:", grid_search.best_params_)
-#print("Best RMSE:", np.sqrt(-grid_search.best_score_))
+print("Best params:", grid_search.best_params_)
+print("Best RMSE:", np.sqrt(-grid_search.best_score_))"""
 
 #Best params for rbf: {'C': 100, 'epsilon': 0.5, 'kernel': 'rbf'} =  97906.83682336772 
-#Best params for linear: {'C': 100, 'epsilon': 0.1, 'kernel': 'linear'} = 70572.48002339934
+#Best params for linear: {'C': 100, 'epsilon': 0.1, 'kernel': 'linear'} = 70158.46280486112
 
 #Best SVR predictor is linear with C = 100, e = 0.1
+
 
 
 
@@ -157,3 +179,17 @@ tree_rmse_scores = np.sqrt(-scores)
 print("Scores:", tree_rmse_scores)
 print("Mean:", tree_rmse_scores.mean())
 print("Standard deviation:", tree_rmse_scores.std())
+
+# Data (replace with your actual numbers)
+models = ["LR", "DT", "SVM-l", "SVM-n", "RF", "SVM_p"]
+means = [69104, 71629, 70158, 96268, 50266, 107249]
+stds = [2880, 2914, 2004, 1960, 2312, 8355]
+
+x = np.arange(len(models))
+
+plt.figure(figsize=(8,5))
+plt.bar(x, means, yerr=stds, capsize=7, color=["skyblue","orange","green","red","purple", "hotpink"])
+plt.xticks(x, models)
+plt.ylabel("RMSE")
+plt.title("Comparison of Models (Mean RMSE ± Std)")
+plt.show()
